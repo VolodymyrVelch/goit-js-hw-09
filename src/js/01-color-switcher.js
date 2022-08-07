037d433b7ea0775506e0c6a699f02aa773ef07a0
Volodymyr Velch <velch83@gmail.com>
2022-08-07 22:10:53 +0300

    js-hw-09-task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const body = document.querySelector('body');
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
 
+buttonStart.addEventListener('click', onBtnStart);
+function onBtnStart(e) {
+  if (buttonStart) {
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+  }
+  intervalChangeColor = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+buttonStop.addEventListener('click', () => {
+  if (buttonStop) {
+    buttonStop.disabled = true;
+    buttonStart.disabled = false;
+  }
+  clearInterval(intervalChangeColor);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// <button type="button" data-start>Start</button>
+// <button type="button" data-stop>Stop</button>
